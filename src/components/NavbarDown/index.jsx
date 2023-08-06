ca29febe58f33cc45453922161fb376fd214c11b
altynovbayel <altynovich30@gmail.com>
2023-08-06 16:27:00 +0600

    banner fix

--- src/components/NavbarDown/index.jsx
+++ src/components/NavbarDown/index.jsx
@@ -11,7 +11,7 @@ const NavbarDown = () => {
         <ul>
           {
             navList.map(item => (
-              <li>
+              <li key={item.id}>
                 <Link to={item.route}>
                   {item.title}
                 </Link>
