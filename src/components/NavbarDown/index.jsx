af4d90225e745469c25d2acbbb2864cae5523670
altynovbayel <altynovich30@gmail.com>
2023-08-08 02:47:23 +0600

    sidebar fix

--- src/components/NavbarDown/index.jsx
+++ src/components/NavbarDown/index.jsx
@@ -1,34 +1,40 @@
 import React from 'react'
 import c from './NavbarDown.module.scss'
 import { navList } from '../../utils'
-import { Link, useNavigate } from 'react-router-dom'
+import {NavLink, useLocation } from 'react-router-dom'
 import search from '../../images/search.svg'
 import burger from '../../images/burger.svg'
 import logo from '../../images/logo-white.svg'
 import Sidebar from './Sidebar'
 
 const NavbarDown = () => {
-  const navigate = useNavigate()
+  const [sidebarActive, setSidebarActive] = React.useState(false)
+  const path = useLocation().pathname
+  
+  React.useEffect(() => {
+    setSidebarActive(false)
+  }, [path])
+  
   return (
-    <div className={c.navbar}>
-      <div className={c.container}>
-        <ul>
-          {
-            navList.map(item => (
-              <li key={item.id}>
-                <Link to={item.route}>
-                  {item.title}
-                </Link>
-              </li>
-            ))
-          }
-        </ul>
-        <div className={c.logo}>
-          <img src={logo} alt="logo" onClick={() => navigate('/')} />
-        </div>
-        <div className={c.right}>
-          <div className={c.search}>
-            <img src={search} alt="img" />
+     <>
+      <div className={c.navbar}>
+        <div className={c.container}>
+          <ul>
+            {
+              navList.map(item => (
+                <li key={item.id}>
+                  <NavLink 
+                    to={item.route} 
+                    className={({ isActive }) => (isActive ? c.active : 'inactive')}
+                  >
+                    {item.title}
+                  </NavLink>
+                </li>
+              ))
+            }
+          </ul>
+          <div className={c.logo}>
+            <img src={logo} alt="" />
           </div>
           <div className={c.right}>
             <div className={c.search}>
