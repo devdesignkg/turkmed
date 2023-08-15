c3b0866359e2cfb51209c60399b587fd9c47dbf7
altynovbayel <altynovich30@gmail.com>
2023-08-16 03:31:04 +0600

    footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,29 +3,38 @@ import c from './Footer.module.scss'
 import insta from '../../images/instagram.svg'
 import youtube from '../../images/youtube.svg'
 import whatsapp from '../../images/whatsapp.svg'
+import devLogo from '../../images/DevDesign.svg'
 
 const Footer = () => {
   return (
-    <div className={c.footer}>
-      <div className={c.container}>
-        <div className={c.footer_bottom}>
-          <div className={c.bottom_flex}>
-            <p>© 2023 TURKMED - гемодиализ</p>
-            <ul>
-              <li>
-                <img src={insta} alt="insta" />
-              </li>
-              <li>
-                <img src={youtube} alt="youtube" />
-              </li>
-              <li>
-                <img src={whatsapp} alt="whatsapp" />
-              </li>
-            </ul>
+    <>
+      <div className={c.footer}>
+        <div className={c.container}>
+          <div className={c.footer_bottom}>
+            <div className={c.bottom_flex}>
+              <p>© 2023 TURKMED - гемодиализ</p>
+              <ul>
+                <li>
+                  <img src={insta} alt="insta" />
+                </li>
+                <li>
+                  <img src={youtube} alt="youtube" />
+                </li>
+                <li>
+                  <img src={whatsapp} alt="whatsapp" />
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <div className={c.our_footer}>
+        <p>Сайт разработан:</p>
+        <a href="https://devdesign.site/">
+          <img src={devLogo} alt="devDesign" />
+        </a>
+      </div>
+    </>
   )
 }
 
