35ce6c3f829fffa34c4bf8dacb956bee9d898fb5
altynovbayel <altynovich30@gmail.com>
2023-09-05 00:37:02 +0600

    add link

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import c from './Footer.module.scss'
 import insta from '../../images/instagram.svg'
-import youtube from '../../images/youtube.svg'
 import whatsapp from '../../images/whatsapp.svg'
 import devLogo from '../../images/DevDesign.svg'
+import {BsFacebook} from 'react-icons/bs'
+
 
 const Footer = () => {
   return (
@@ -15,10 +16,12 @@ const Footer = () => {
               <p>© 2023 TURKMED - гемодиализ</p>
               <ul>
                 <li>
-                  <img src={insta} alt="insta" />
+                  <a href="https://www.instagram.com/turkmed_osh" target="_blank">
+                    <img src={insta} alt="insta" />
+                  </a>
                 </li>
                 <li>
-                  <img src={youtube} alt="youtube" />
+                  <BsFacebook/>
                 </li>
                 <li>
                   <img src={whatsapp} alt="whatsapp" />
@@ -30,7 +33,7 @@ const Footer = () => {
       </div>
       <div className={c.our_footer}>
         <p>Сайт разработан:</p>
-        <a href="https://devdesign.site/">
+        <a href="https://devdesign.site/" target="_blank">
           <img src={devLogo} alt="devDesign" />
         </a>
       </div>
